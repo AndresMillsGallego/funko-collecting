584ec6116714f426c13bba01ee1fec065dc13104
Andres Mills Gallego <geneseewoodworking@gmail.com>
2021-09-22 14:35:13 -0700

    trying to fix this rate us button

--- app4.js
+++ app4.js
@@ -3,9 +3,11 @@ function userRating() {
             for (let i = 0; i < rating; i++) {    
                if(rating <= 5) { 
                 document.write('<img id="freddie" src="https://covetly-prod-images.azureedge.net/8d402013-3dc9-4aab-a25e-556c7825a7bc-256x500.jpg">');
-            } else {
-                alert("Please enter a number between 1-5") 
+            } else if (rating > 5) {
+                alert("Please enter a number between 1-5"); 
                 return false;
+            } else {
+                alert("Error");
             }
         }
     };
