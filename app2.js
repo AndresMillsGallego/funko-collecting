c2af5e9bb97a9d1af0ed8e98f1c00201814cbc5e
Andres Mills Gallego <geneseewoodworking@gmail.com>
2021-09-22 11:58:13 -0700

    Tweaked the page for Lab 08

--- app2.js
+++ app2.js
@@ -1,17 +1,18 @@
 function askColor() {
     
     let colorChoice = prompt("Do you like this color? Yes or No?")
-        if (colorChoice.toLowerCase() == "yes") {
-            alert("Me too")
+ 
+            if (colorChoice.toLowerCase() == "yes") {
+                alert("Me too")
             return true;
-        }
-         else if (colorChoice.toLowerCase() == "no") {
-         document.body.style.backgroundColor = ("#1E90FF");
-         return true;
+
+            } else if (colorChoice.toLowerCase() == "no") {
+                document.body.style.backgroundColor = ("#1E90FF");
+            return true;
         
         }else{
             alert("Come again?");
-            askColor();
-            return false
+                askColor();
+            return false;  
         }
     };
---
+++ app4.js
@@ -0,0 +1,7 @@
+function userRating() {
+        let rating = prompt("How would you rate us? On a scale of 1-5 Freddies?");
+            for (let i = 0; i < rating; i++) {    
+                document.write('<img src="https://covetly-prod-images.azureedge.net/8d402013-3dc9-4aab-a25e-556c7825a7bc-256x500.jpg">');
+              
+    }
+};
